a4153e8759e199238f332724ab2ef85509c36505
aydenyipcs <aydenyip56@gmail.com>
2024-01-04 20:00:38 -0500

    setup

---
+++ app/components/ThemeToggle.jsx
@@ -0,0 +1,10 @@
+"use client"
+import { useEffect, useState } from "react";
+import { FaMoon } from "react-icons/fa";
+import { BsSunFill } from "react-icons/bs";
+
+const ThemeToggle = () => {
+    
+}
+
+export default ThemeToggle;
